203aa5b426863ecce5ee9f5040d77f2910dad80d
mario welch <mario.welch01@gmail.com>
2022-07-12 20:03:21 -0500

    day 01 of code

---
+++ day01-bandnamegenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!!!")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("The name of your band is: " + city + " " + pet)

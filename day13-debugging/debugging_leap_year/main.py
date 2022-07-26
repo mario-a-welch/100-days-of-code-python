c48d20f5e718042a6c6d146f80f8979d73a7ece5
mario welch <mario.welch01@gmail.com>
2022-07-26 14:09:20 -0500

    day13

---
+++ day13-debugging/debugging_leap_year/main.py
@@ -0,0 +1,14 @@
+# year = input("Which year do you want to check?") produces type error when comparing it to modulus
+# operator of 4, must be converted to int
+year = int(input("Which year do you want to check?"))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap year.")
+else:
+    print("Not leap year.")
---
+++ day13-debugging/debugging_odd_or_even/main.py
@@ -0,0 +1,7 @@
+number = int(input("Which number do you want to check? "))
+
+# if number % 2 = 0: added additional equal sign to compare result of modulus operator to check for remainder
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
